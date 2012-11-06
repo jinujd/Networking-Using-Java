d9d51b116a3ee9237794352399b01365716029ac
jinujd <jinujosephdaniel@gmail.com>
2012-11-07 00:30:15 +0530

    Update broadCasting/Client.java

--- broadCasting/Client.java
+++ broadCasting/Client.java
@@ -3,22 +3,23 @@ import java.io.*;
 
 class Client {
     private static Socket server;
-	private static BufferedReader buffer;
-	private static String data;
-	public static void main(String args[]) {
-		try {
-			server = new Socket("localhost",2000);
-			data = "";
-			buffer = new BufferedReader(new InputStreamReader(server.getInputStream()));
-			do {
-			    System.out.println(data);
-			} while(!(data = buffer.readLine()).equals("stop"));
-			buffer.close();
-			server.close();
-		} catch(Exception e) {
-			System.out.println("Could not create client" + e);
-		}
+    private static BufferedReader buffer;
+    private static String data;
+    public static void main(String args[]) {
+        try {
+            server = new Socket("localhost", 2000);
+            System.out.println("Connected to server...");
+            buffer = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            while (!(data = buffer.readLine()).equals("stop")) {
+                System.out.println(data);
+            }
+            buffer.close();
+            server.close();
+            System.out.println("Disconnected from server...");
+        } catch (Exception e) {
+            System.out.println("Could not create client" + e);
+        }
 
-	}
+    }
 
 }
